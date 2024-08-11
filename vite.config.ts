113139542cbf071d359192ed559a0ba6251fa9ab
Olly Rowe <oliver.rowe101@gmail.com>
2024-08-11 22:29:39 +0100

    fix: update vite config to set base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,18 @@
 import { defineConfig } from "vite";
+import checker from "vite-plugin-checker";
 
 export default defineConfig({
+  plugins: [
+    checker({
+      typescript: true,
+      eslint: {
+        lintCommand: "eslint . --ext ts,tsx",
+      },
+    }),
+  ],
   esbuild: {
     jsxInject: 'import { createElement } from "@ollyrowe/core"',
     jsxFactory: "createElement",
   },
+  base: "/2048/",
 });
